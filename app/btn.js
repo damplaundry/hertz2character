c20e125abbecf3fee69cb506e00b92db7f573969
damplaundry <144713235+damplaundry@users.noreply.github.com>
2024-05-31 17:58:23 +0000

    fried chicken fingers

--- app/btn.js
+++ app/btn.js
@@ -2,3 +2,8 @@ const button = document.querySelector('#button-clear');
 button.addEventListener('click', () => {
     document.querySelector('#textareaEl').value = "";
 });
+
+const buttonCopy = document.querySelector('#button-clear');
+buttonCopy.addEventListener('click', () => {
+    document.querySelector('#textareaEl').value = "";
+});
