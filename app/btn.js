d841bec4d0105843e3779f9fc96c986ecf5341c1
damplaundry <144713235+damplaundry@users.noreply.github.com>
2024-06-05 16:00:43 +0000

    clear button works now

--- app/btn.js
+++ app/btn.js
@@ -1,9 +1,5 @@
 const button = document.querySelector('#button-clear');
 button.addEventListener('click', () => {
     document.querySelector('#textareaEl').value = "";
-});
-
-const buttonCopy = document.querySelector('#button-clear');
-buttonCopy.addEventListener('click', () => {
-    document.querySelector('#textareaEl').value = "";
+    outputString = "";
 });
