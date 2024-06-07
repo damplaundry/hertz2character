25c73dc821cb59349e0d4fbcee6ddf207c2d992c
damplaundry <144713235+damplaundry@users.noreply.github.com>
2024-06-07 16:51:00 +0000

    css changes

--- app/btn.js
+++ app/btn.js
@@ -1,5 +1,14 @@
-const button = document.querySelector('#button-clear');
-button.addEventListener('click', () => {
+const buttonCl = document.querySelector('#button-clear');
+buttonCl.addEventListener('click', () => {
     document.querySelector('#textareaEl').value = "";
     outputString = "";
+});
+
+const buttonCo = document.querySelector('#button-copy');
+buttonCo.addEventListener('click', async () => {
+    try {
+        await navigator.clipboard.writeText(outputString);
+      } catch (error) {
+        console.error(error.message);
+      }
 });
